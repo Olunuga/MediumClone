1b46bd7f6f520a3b421148043af0ff731900d780
Mayowa Olunuga <myorh@myorhs-macbook.local>
2018-09-17 07:29:30 +0100

    Initial Commit

---
+++ MediumClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediumClone
+//
+//  Created by OLUNUGA Mayowa on 17/09/2018.
+//  Copyright © 2018 OLUNUGA Mayowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
