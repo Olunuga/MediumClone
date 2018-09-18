10984a0a67e978bd3829ac8efcdbeedaed81c92f
Mayowa Olunuga <myorh@myorhs-macbook.local>
2018-09-18 09:26:17 +0100

    Start home page view, Add simple story cell

---
+++ MediumClone/Module/Home/HomeViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HomeViewController.swift
+//  MediumClone
+//
+//  Created by OLUNUGA Mayowa on 18/09/2018.
+//  Copyright © 2018 OLUNUGA Mayowa. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UINib(nibName: "WideStoryViewCell", bundle: nil), forCellReuseIdentifier: "wideCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 240.0
+        tableView.separatorColor = UIColor(white: 0.5, alpha: 0.3)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8;
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "wideCell", for: indexPath) as! WideStoryViewCell
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
+    
+}
---
+++ MediumClone/Module/Home/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  MediumClone
+//
+//  Created by OLUNUGA Mayowa on 18/09/2018.
+//  Copyright © 2018 OLUNUGA Mayowa. All rights reserved.
+//
+
+import Foundation
